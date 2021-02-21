d96aee9558c2e5e14b67a3853272d08d2947e02b
Marcin <marcin_privbate@wp.pl>
2021-02-21 11:56:23 +0100

    [ms-2030] add enzyme + stylelint + unit test

--- src/components/SignalLabel/SignalLabel.jsx
+++ src/components/SignalLabel/SignalLabel.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { PropTypes } from 'prop-types';
 
 import './SignalLabel.scss';
 
 export const SignalLabel = ({ signal }) => (
 	<span className="SignalLabel">
-		{ signal}
+		{signal}
 	</span>
 )
 
