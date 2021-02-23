9bf9ad7aaa1aafc0faf3d0ee583174ea4670f592
Marcin Gierczak <marcin_private@wp.pl>
2021-02-23 21:16:21 +0100

    [ms-2030] add enzyme + stylelint + unit test (#33)
    Co-authored-by: Marcin <marcin_privbate@wp.pl>

---
+++ src/components/SignalLabel/SignalLabel.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { SignalLabel } from '../SignalLabel/SignalLabel';
+import { signals } from '../../enums/signals.enum';
+
+describe('SignalLabel', () => {
+
+  it('renders button correctly', () => {
+    const wrapper = shallow(
+      <SignalLabel signal={signals.S1} />
+    );
+  
+    const content = wrapper.find('.SignalLabel').text().includes(signals.S1);
+
+    expect(content).toBe(true);
+  });
+
+});
