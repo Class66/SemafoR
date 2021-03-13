2e88d51669e6b1289dfeba0af1222428da3a1a7e
Marcin Gierczak <marcin_private@wp.pl>
2021-03-13 18:05:41 +0100

    [ms-2060] fix ESLint rules (#35)

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Required to enable ES6 Modules in NodeJS
 // https://www.wisdomgeek.com/development/web-development/javascript/how-to-import-export-es6-modules-in-node/
+// eslint-disable-next-line no-native-reassign
 require = require("esm")(module/*, options*/);
 module.exports = require("./semaphore.js");
